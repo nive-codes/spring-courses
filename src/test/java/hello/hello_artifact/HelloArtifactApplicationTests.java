bcb55c16bd22de7c62671ad0de66e40175d7f2fe
chs <chs@sotongfive.kr>
2024-11-20 16:52:16 +0900

    Initial commit fist share

---
+++ src/test/java/hello/hello_artifact/HelloArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hello_artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
