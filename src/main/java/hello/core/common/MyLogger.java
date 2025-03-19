7babe8719f61974223d03f00a2f2a8494b783cf4
nive <nive.codes@gmail.com>
2025-03-19 12:49:22 +0900

    web scope (request) test

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,37 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+//생존범위는 고객 접속 후 나갈때 까지인데, spring 컨테이너가 뜨는 시점에는 요청이 없으므로 오류가 발생
+@Component
+@Scope(value = "request")
+
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "] " + "["+requestURL+"] "+message);
+    }
+
+    @PostConstruct
+    public void init(){
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+    }
+
+    @PreDestroy
+    public void close(){
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
+    }
+}
