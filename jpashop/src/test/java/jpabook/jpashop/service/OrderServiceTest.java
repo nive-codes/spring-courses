0253d0625304273d633cc0a62619134a61a4fd99
nive <nive.codes@gmail.com>
2025-03-27 18:53:29 +0900

    수량 예외처리 테스트 소스 작성

--- jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -6,15 +6,15 @@ import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
-import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.respository.OrderRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.util.*;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Fail.fail;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 
@@ -30,16 +30,9 @@ class OrderServiceTest {
     @Test
     public void 상품주문() throws Exception {
         //given
-        Member member = new Member();
-        member.setName("회원1");
-        member.setAddress(new Address("서울","강가","123-123"));
-        em.persist(member);
+        Member member = createMember();
 
-        Book book = new Book();
-        book.setName("시골 JPA");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
-        em.persist(book);
+        Book book = createBook("시골 JPA", 10000, 10);
 
         int orderCount = 2;
         System.out.println("book 값은? : " + book.getStockQuantity());
@@ -58,6 +51,25 @@ class OrderServiceTest {
 
     }
 
+
+
+    @Test
+    public void 상품주문_재고수량초과() throws Exception {
+        //given
+        Member member = createMember();
+        Book book = createBook("시골 JPA", 10000, 10);
+
+
+        
+        //when
+        int orderCount = 11;
+
+        //then
+        Throwable e = Assertions.assertThrows(NotEnoughStockException.class,() -> orderService.order(member.getId(), book.getId(), orderCount));
+        System.out.println("예외 체크 : " + e.getMessage());
+
+    }
+
     @Test
     public void 주문취소() throws Exception {
         //given
@@ -65,13 +77,22 @@ class OrderServiceTest {
         //then
     }
 
-     @Test
-    public void 상품주문_재고수량초과() throws Exception {
-         //given
-         //when
-         //then
-     }
+    private Book createBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
+    }
 
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울","강가","123-123"));
+        em.persist(member);
+        return member;
+    }
 
 
 }
