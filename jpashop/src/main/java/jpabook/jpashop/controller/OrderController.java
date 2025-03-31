5f7f9dea71a86dee248d8f39a13a0fedbd19e63c
nive <nive.codes@gmail.com>
2025-03-31 14:47:46 +0900

    주문 로직 생성

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,56 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+/**
+ * @author hosikchoi
+ * @class OrderController
+ * @desc order 관련 controller
+ * @since 2025-03-31
+ */
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final ItemService itemService;
+    private final MemberService memberService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long MemberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count){
+
+        orderService.order(MemberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+//    @GetMapping("/orders")
+//    public String orders(Model model){
+//        return "order/orderList";
+//    }
+}
